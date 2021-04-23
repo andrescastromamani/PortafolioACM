9e5d3daec06dfd9d109a46e089b4b43652106fc1
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-04-22 21:06:13 -0400

    Activamos el menu de navegacion

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,15 @@ const showMenu = (toggleId, navId)=>{
     }
 }
 showMenu('nav-toggle','nav-menu')
+
+/*Active and remove menu*/
+const navlink = document.querySelectorAll('.nav-link')
+function linkAction(){
+    //Active link
+    navlink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
+    //Remove menu mobile
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+navlink.forEach(n => n.addEventListener('click',linkAction))
