519c398c9ae50b3d1aea9a03e85a61934c122cdc
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-04-29 21:56:03 -0400

    Agregamos las animaciones a las secciones

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,3 +22,33 @@ function linkAction(){
     navMenu.classList.remove('show')
 }
 navlink.forEach(n => n.addEventListener('click',linkAction))
+
+/*Scroll Animation */
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+/*Scroll Home */
+sr.reveal('.home-title',{})
+sr.reveal('.button',{delay:200})
+sr.reveal('.home-img',{delay:400})
+sr.reveal('.social-icon',{interval: 200})
+
+/*Scroll About */
+sr.reveal('.about-img',{})
+sr.reveal('.about-subtitle',{delay:200})
+sr.reveal('.about-text',{delay:400})
+
+/**Scroll Skills */
+sr.reveal('.skills-subtitle',{})
+sr.reveal('.skills-text',{delay:200})
+sr.reveal('.skills-data',{interval:200})
+sr.reveal('.skills-img',{delay: 400})
+
+/**Scroll Work */
+sr.reveal('.work-img',{interval:200})
+
+/**Scroll Contact */
+sr.reveal('.contact-input',{interval:200})
